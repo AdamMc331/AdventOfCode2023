3c4f28cd35594e0f6050aeb3665d9529da762594
Adam McNeilly <amcneilly331@gmail.com>
2023-12-06 20:12:13 -0500

    Day one complete!

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,75 @@
 fun main() {
+    val numWords = listOf(
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+    )
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.sumOf { line ->
+            val firstDigit = line.first(Char::isDigit).digitToInt()
+            val lastDigit = line.last(Char::isDigit).digitToInt()
+
+            (firstDigit * 10) + lastDigit
+        }
     }
 
+    fun getFirstDigit(line: String): Int {
+        line.forEachIndexed { index, c ->
+            if (c.isDigit()) {
+                return c.digitToInt()
+            }
+
+            numWords.forEachIndexed { numStringIndex, numString ->
+                if (line.substring(index).startsWith(numString)) {
+                    return numStringIndex + 1
+                }
+            }
+        }
+
+        return -1
+    }
+
+    fun getLastDigit(line: String): Int {
+        for (index in line.lastIndex downTo 0) {
+            val c = line[index]
+
+            if (c.isDigit()) {
+                return c.digitToInt()
+            }
+
+            numWords.forEachIndexed { numStringIndex, numString ->
+                if (line.substring(index).startsWith(numString)) {
+                    return numStringIndex + 1
+                }
+            }
+        }
+
+        return -1
+    }
+
+    /**
+     * Ideally for part 2, we just reprocess the data into a format that works well with part 1
+     * Replace any string words with the digit they represent.
+     */
     fun part2(input: List<String>): Int {
-        return input.size
+        return input.sumOf { line ->
+            val firstDigit = getFirstDigit(line)
+            val lastDigit = getLastDigit(line)
+
+            (firstDigit * 10) + lastDigit
+        }
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part2(testInput) == 281)
 
     val input = readInput("Day01")
     part1(input).println()
